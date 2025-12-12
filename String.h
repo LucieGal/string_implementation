f099e0956f7023168cea49f23f31fa7c2e284c9b
Hugues DREYFUS <hugues.dreyfus@insa-lyon.fr>
2025-12-12 17:25:57 +0100

    Cleaner String.h

--- String.h
+++ String.h
@@ -4,24 +4,20 @@ class String {
     public:
         String();
         String(char* input_string);
-        ~String();
         String(const String& c);
+        ~String();
 
         char* str();
-
-        size_t capacity();
         bool empty();
-        void reserve(size_t n);
-
+        size_t capacity();
         size_t size();
-
-
         size_t max_size();
+
+        void reserve(size_t n);
         void resize(size_t new_size, char filler);
 
         void operator=(const char* s);
         // void operator+(const String& str1, const String& str2);
-
         
     private:
         size_t capacity_ = 0;
@@ -29,5 +25,4 @@ class String {
         char* str_;
 
         static const size_t max_size_ = 100;
-
 };
