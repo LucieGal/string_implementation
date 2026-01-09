b561e2c3208b43f261a70c08ca769e3c7c2b729c
lucie galea <lucie.galea@insa-lyon.fr>
2026-01-09 14:27:40 +0100

    cmake builder

---
+++ build/CMakeFiles/string.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucie/Documents/Insa/4BIM/projet_groupe_info5/String.cpp" "CMakeFiles/string.dir/String.cpp.o" "gcc" "CMakeFiles/string.dir/String.cpp.o.d"
+  "/home/lucie/Documents/Insa/4BIM/projet_groupe_info5/main.cpp" "CMakeFiles/string.dir/main.cpp.o" "gcc" "CMakeFiles/string.dir/main.cpp.o.d"
+  "" "string" "gcc" "CMakeFiles/string.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
