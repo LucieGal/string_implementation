1df4adaddd2fd5836bd047ab46f844a0aa8729a6
hdreyfus <huguesdreyfus@proton.me>
2026-01-11 22:51:00 +0100

    Created most of test sudent B, updated all three ctors, cleaned student A tests and delete

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "String.h"
 #include <iostream>
+#include <cassert>
+#include <cstring>
 
 using std::cout;
 using std::endl;
 
 int test_student_A();
+int test_student_B();
 
 int main(){
 
@@ -52,11 +55,12 @@ int main(){
     cout << custom.c_str() << endl;
     cout << custom.size() << endl;
     cout << custom.max_size() << endl;
-    delete test;
-    delete s;
+    //delete[] test;
+    delete[] s;
 
 	
 	test_student_A();
+	test_student_B();
 	
 	return 0;     
 	// j'ai rajouté des delete mas je pense que des copy d'elements sont mal fait parceque j'ai un message de double free 
@@ -88,7 +92,7 @@ int test_student_A(){
 	
 	std::cout<<"Test d'opérator=(char)"<<std::endl;
 	String Str;
-	char* c = new char('m');
+	char* c = new char[2]{'m', '\0'};
 	std::cout<<"Notre str est "<<Str.c_str()<<std::endl;
 	std::cout<<"on le transforme en str m"<<std::endl;
 	Str = c;
@@ -108,9 +112,106 @@ int test_student_A(){
 	std::cout<<std::endl;
 
 
-	delete c;
-	delete char_array;
+	delete[] c;
+	delete[] char_array;
 	return 0;
+}
 
+void test_custom_ctor(){
+	const char* char1 = "Hello World!"; //Lenght is 12.
+	String str1(char1);
+	assert(str1.size() == 12);
 
+	String str1b("Hello World!");
+	assert(str1b.size() == 12);
+	
+	const char* char2 = "";
+	String str2(char2);
+	assert(str2.size() == 0);
+	assert(str2.empty());
+
+	try{
+		const char* char3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent est tellus, placerat nec lacus nec, cursus vestibulum."; //Is more than 100 characters-long
+		String str3(char3);
+		assert(false);
+	}
+	catch (const std::length_error&){
+		assert(true);
+	}
+}
+
+void test_length(){
+	String str1("Hello");
+	assert(str1.length() == 5);
+
+	String str2("");
+	assert(str2.length() == 0);
+
+	String str3(str1);
+	assert(str3.length() == 5);
+}
+
+void test_max_size(){
+	String str1("Hello");
+	assert(str1.max_size() == 100);
+
+	String str2;
+	assert(str2.max_size() == 100);
+
+	String str3(str1);
+	assert(str3.max_size() == 100);
+}
+
+void test_resize(){
+	String str1("Hello");
+
+	str1.resize(15); //purposefully outside of capacity_
+	assert(str1.length() == 15);
+	assert(str1.c_str()[5] =='\0');
+
+	str1.resize(2);
+	assert(str1.length() == 2);
+	assert(strcmp(str1.c_str(), "He") == 0);
+
+	str1.resize(8, 'x');
+	assert(str1.length() == 8);
+	assert(strcmp(str1.c_str(), "Hexxxxxx") == 0);
+
+	str1.resize(0);
+	assert(str1.length() == 0);
+	assert(str1.empty());
+
+	try{
+		str1.resize(101);
+		assert(false);
+	}
+	catch (const std::length_error&){
+		assert(true);
+	}
+}
+
+void test_operatorBeq(){
+
+}
+
+void test_operatorBpl(){
+
+}
+
+int test_student_B(){
+	cout << "\n###### Testing Student B ######" << endl;
+	test_custom_ctor();
+	cout << "Testing of the custom ctor: passed!" << endl;
+	test_length();
+	cout << "Testing of the length function: passed!" << endl;
+	test_max_size();
+	cout << "Test of the max_size function: passed!" << endl;
+	test_resize();
+	cout << "Test of the resize function: passed!" << endl;
+	test_operatorBeq();
+	cout << "Test of the = operator (string&) function: passed!" << endl;
+	test_operatorBpl();
+	cout << "Test of the + operator (srting& + string&): passed!" << endl;
+	
+	return 0;
 }
